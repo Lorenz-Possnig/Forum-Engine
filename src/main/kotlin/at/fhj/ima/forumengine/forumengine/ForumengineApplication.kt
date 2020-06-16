36cafa62f6033cdf576afc662cf1fdb9336d521f
unknown <lorenz.possnig@edu.fh-joanneum.at>
2020-06-16 11:36:22 +0200

    Initial Commit

---
+++ src/main/kotlin/at/fhj/ima/forumengine/forumengine/ForumengineApplication.kt
@@ -0,0 +1,11 @@
+package at.fhj.ima.forumengine.forumengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumengineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumengineApplication>(*args)
+}
